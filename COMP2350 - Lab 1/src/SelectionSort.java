3925afb2e3979a5887682b3477fcfcfc7723e4c1
Bryan DeFaria <defariab@wit.edu>
2023-01-26 12:51:31 -0500

    Selection Sort fix

--- COMP2350 - Lab 1/src/SelectionSort.java
+++ COMP2350 - Lab 1/src/SelectionSort.java
@@ -1,83 +1,80 @@
-import java.io.BufferedReader ;
-import java.io.InputStreamReader ;
 import java.util.Scanner ;
 
 /**
- * 
- * 
  * @author Laura Wysocki and Bryan DeFaria
- * @version 1.0.0 2023-01-26 Initial implementation
  *
+ * @version 1.0.0 2023-01-26 Initial implementation
  */
 public class SelectionSort
     {
-    
+
     /**
-     * 
-     * 
      * @param array
-     * @return 
+     *
+     * @return
      */
     public static int[] selectionSort( final int[] array )
         {
-        int min = array[0] ;
+        int min = array.length ;
 
-        for ( int i = 0 ; i < array.length ; i++ )
+        for ( int i = 0 ; i < ( array.length - 1 ) ; i++ )
             {
-            min = array[i] ;
-            for (int j = i + 1 ; j < array.length ; j++)
+            min = i ;
+            for ( int j = i + 1 ; j < array.length ; j++ )
                 {
-                if (array[j] < array[min])
+                if ( array[ j ] < array[ min ] )
                     {
-                    min = array[j] ;
-                    
+                    min = j ;
+
                     }
 
                 }
-            int temp = array[min] ;
-            array[min] = array[i] ;
-            array[i] = temp ;
+
+            int temp = array[ min ] ;
+            array[ min ] = array[ i ] ;
+            array[ i ] = temp ;
 
             } // end for
+
         return array ;
 
         }   // end selectionSort()
 
 
     /**
-     * 
-     * 
      * @param args
      */
     public static void main( String[] args )
         {
-        Scanner input = new Scanner( System.in ) ;
-        
-        System.out.printf( "Please input size of array: ") ;
-        int arraySize = input.nextInt() ;
-        
-        Scanner input2 = new Scanner( System.in ) ;
-        int[] theArray = new int[arraySize] ;
-        System.out.printf( "%nPlease input your array in the format of integers seperated by spaces: " ) ;
-        
-        for (int i = 0 ; i < arraySize; i++)
+        try ( Scanner input = new Scanner( System.in ) )
             {
-            theArray[i] = input2.nextInt() ;
+
+            System.out.printf( "Please input size of array: " ) ;
+            int arraySize = input.nextInt() ;
+
+            int[] theArray = new int[ arraySize ] ;
+            System.out.printf( "%nPlease input your array in the format of integers seperated by spaces: " ) ;
+
+            for ( int i = 0 ; i < arraySize ; i++ )
+                {
+                theArray[ i ] = input.nextInt() ;
+
+                }
+
+            selectionSort( theArray ) ;
+
+            String s = "" ;
+            System.out.printf( "%nYour sorted array: " ) ;
+            for ( int a : theArray )
+                {
+                System.out.printf( s + a ) ;
+                s = ", " ;
+
+                }
+
             }
-        
-        selectionSort( theArray ) ;
-        
-        String s = "" ;
-        System.out.printf( "%nYour sorted array: ");
-        for (int a : theArray)
-            {
-            System.out.printf( s + a) ;
-            s = ", " ;
-            }
-        
-        
 
         }
 
     }
-   // end class SelectionSort
+// end class SelectionSort

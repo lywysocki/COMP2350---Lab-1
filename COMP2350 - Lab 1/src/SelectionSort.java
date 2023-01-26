5c46615ac0e16cbefff30aade2f5e046f7e9403a
lywysocki <lywysocki@gmail.com>
2023-01-26 09:38:14 -0500

    here

--- COMP2350 - Lab 1/src/SelectionSort.java
+++ COMP2350 - Lab 1/src/SelectionSort.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader ;
+import java.io.InputStreamReader ;
 import java.util.Scanner ;
 
 /**
@@ -23,16 +25,19 @@ public class SelectionSort
         for ( int i = 0 ; i < array.length ; i++ )
             {
             min = array[i] ;
-            for (int j = i + 1 ; j < array.length - 1 ; j++)
+            for (int j = i + 1 ; j < array.length ; j++)
                 {
-                if (array[j] < array[i])
+                if (array[j] < array[min])
                     {
                     min = array[j] ;
+                    
                     }
+
                 }
-            int temp = array[i] ;
-            array[i] = min ;
-            min = temp ;
+            int temp = array[min] ;
+            array[min] = array[i] ;
+            array[i] = temp ;
+
             } // end for
         return array ;
 
@@ -51,7 +56,26 @@ public class SelectionSort
         System.out.printf( "Please input size of array: ") ;
         int arraySize = input.nextInt() ;
         
-        int[] theArray = new int[]
+        Scanner input2 = new Scanner( System.in ) ;
+        int[] theArray = new int[arraySize] ;
+        System.out.printf( "%nPlease input your array in the format of integers seperated by spaces: " ) ;
+        
+        for (int i = 0 ; i < arraySize; i++)
+            {
+            theArray[i] = input2.nextInt() ;
+            }
+        
+        selectionSort( theArray ) ;
+        
+        String s = "" ;
+        System.out.printf( "%nYour sorted array: ");
+        for (int a : theArray)
+            {
+            System.out.printf( s + a) ;
+            s = ", " ;
+            }
+        
+        
 
         }
 
